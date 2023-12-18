85e4c0c42db7c9ea7309c43d9ed94522b59583c1
Francisco Cruz <2020107844@uac.pt>
2023-12-18 23:07:17 +0000

    sdf

--- untitled/src/domain/InteracaoAlimentarCreator.java
+++ untitled/src/domain/InteracaoAlimentarCreator.java
@@ -2,7 +2,7 @@ package domain;
 
 public class InteracaoAlimentarCreator {
 
-    public static InteracaoAlimentar criar_Interacao_Alimentar(String explicacao, String alimento, String efeito, String referencia_bibliografica, SubstanciaAtiva substanciaAtiva){
+    public static InteracaoAlimentar criar_Interacao_Alimentar(SubstanciaAtiva substanciaAtiva, String explicacao, String alimento, String efeito, String referencia_bibliografica){
 
         InteracaoAlimentar interacaoAlimentar = new InteracaoAlimentar(explicacao, alimento, efeito, referencia_bibliografica, substanciaAtiva);
         return new InteracaoAlimentar(explicacao, alimento, efeito, referencia_bibliografica, substanciaAtiva);
